4418d05b0b9d1653345dbee16feee33fea6ad250
John Patrick Ombao <www.ombaojohnpatrick@gmail.com>
2025-01-07 01:43:29 +0800

    Adding dim_stockitem and dim_supplier

---
+++ models/warehouse/dimensions/dim_stockitem.sql
@@ -0,0 +1,50 @@
+with
+
+stockitem as (
+  select * from {{ ref('stg_mssql__stockitems') }}
+),
+
+packagetype as (
+  select * from {{ ref('stg_mssql__packagetypes') }}
+),
+
+color as (
+  select * from {{ ref('stg_mssql__colors') }}
+),
+
+final as (
+
+  select
+    {{ dbt_utils.generate_surrogate_key(['stockitem.stock_item_id'])}},
+
+    stockitem.stock_item_id,
+    stockitem.stock_item_name,
+    stockitem.unit_price,
+
+    color.color_name,
+    packagetype.package_type_name,
+
+    stockitem.typical_weight_per_unit,
+    stockitem.size,
+    stockitem.tags,
+    stockitem.brand,
+    stockitem.photo,
+    stockitem.barcode,
+    stockitem.taxrate,
+    stockitem.recommended_retail_price,
+    stockitem.quantity_per_outer,
+    stockitem.lead_time_days,
+    stockitem.valid_from,
+    stockitem.valid_to,
+    stockitem.last_edited_by
+
+  from stockitem
+
+  left join packagetype
+    on stockitem.outer_package_id = packagetype.package_type_id
+  left join color 
+    on stockitem.color_id = color.color_id
+
+)
+
+select * from final
